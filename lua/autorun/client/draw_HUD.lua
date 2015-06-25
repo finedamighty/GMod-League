a58f993bd316f1bb40b026536f73be29d0f0e7c4
DeveloperMew <matio01@hot.ee>
2015-06-25 19:39:41 +0300

    Second commit!
    Added some files from other addons. (ALL MINE!!)

---
+++ lua/autorun/client/draw_HUD.lua
@@ -0,0 +1,16 @@
+if CLIENT then
+	hook.Add( "Think", "mew_world_tip", function()
+		if LocalPlayer() != nil then
+			local ply = LocalPlayer()
+			local tr = ply:GetEyeTrace()
+			local pos = tr.HitPos
+			local ent = tr.Entity
+			if ent.isMew == true then -- is it mews?
+				local txt = ent:GetNWString("mew_n_data") -- yes it is!
+				if txt != "mew_no_hint" then
+					AddWorldTip( nil, txt, nil, pos, ent )
+				else end
+			else end
+		else end
+	end)
+end
